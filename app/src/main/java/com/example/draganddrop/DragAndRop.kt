ba228e27753726a2975e09355861e4be8d3fe2a1
tamerlan.mustafayev <tamerlan_94@mail.ru>
2023-03-28 14:56:13 +0400

    code completed

--- app/src/main/java/com/example/draganddrop/DragAndRop.kt
+++ app/src/main/java/com/example/draganddrop/DragAndRop.kt
@@ -3,19 +3,25 @@ package com.example.draganddrop
 import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.consumeAllChanges
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.boundsInWindow
 import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.unit.IntSize
 
 internal val LocalDragTargetInfo = compositionLocalOf { DragTargetInfo() }
+
 //test commit
 @Composable
 fun <T> DragTarget(
     modifier: Modifier = Modifier,
     dataToDrop: T,
+    viewModel: MainViewModel,
     content: @Composable (() -> Unit)
 ) {
     var currentPosition by remember { mutableStateOf(Offset.Zero) }
@@ -29,22 +35,23 @@ fun <T> DragTarget(
             .pointerInput(Unit) {
                 detectDragGesturesAfterLongPress(
                     onDragStart = {
-                        currentState.dayaToDrop = dataToDrop
+                        viewModel.startDragging()
+                        currentState.dataToDrop = dataToDrop
                         currentState.isDragging = true
                         currentState.dragPosition = currentPosition + it
                         currentState.draggableComposable = content
                     },
                     onDrag = { change, dragAmount ->
-                        change.consumeAllChanges()
+                        change.consume()
                         currentState.dragOffset += Offset(dragAmount.x, dragAmount.y)
                     },
                     onDragEnd = {
-                        //viewModel.stopDragging()
+                        viewModel.stopDragging()
                         currentState.dragOffset = Offset.Zero
                         currentState.isDragging = false
                     },
                     onDragCancel = {
-                        //viewModel.stopDragging()
+                        viewModel.stopDragging()
                         currentState.dragOffset = Offset.Zero
                         currentState.isDragging = false
                     }
@@ -60,7 +67,65 @@ fun <T> DropItem(
     modifier: Modifier,
     content: @Composable() (BoxScope.(isInBound: Boolean, data: T?) -> Unit)
 ) {
+    val dragInfo = LocalDragTargetInfo.current
+    val dragPosition = dragInfo.dragPosition
+    val dragOffset = dragInfo.dragOffset
+    var isCurrentDropTarget by remember {
+        mutableStateOf(false)
+    }
+    Box(
+        modifier = modifier
+            .onGloballyPositioned {
+                it.boundsInWindow().let { rect ->
+                    isCurrentDropTarget = rect.contains(dragPosition + dragOffset)
+                }
+            }
+    ) {
+        val data =
+            if (isCurrentDropTarget && !dragInfo.isDragging) dragInfo.dataToDrop as T? else null
+        content(isCurrentDropTarget, data)
+    }
+}
 
+@Composable
+fun DraggableScreen(
+    modifier: Modifier = Modifier,
+    content: @Composable BoxScope.() -> Unit
+) {
+    val state = remember {
+        DragTargetInfo()
+    }
+
+    CompositionLocalProvider(
+        LocalDragTargetInfo provides state
+    ) {
+        Box(
+            modifier = modifier.fillMaxSize()
+        ) {
+            content()
+            if (state.isDragging) {
+                var targetSize by remember {
+                    mutableStateOf(IntSize.Zero)
+                }
+                Box(
+                    modifier = Modifier
+                        .graphicsLayer {
+                            val offset = (state.dragPosition + state.dragOffset)
+                            scaleX = 1.3f
+                            scaleY = 1.3f
+                            alpha = if (targetSize == IntSize.Zero) 0f else .9f
+                            translationX = offset.x.minus(targetSize.width / 2)
+                            translationY = offset.y.minus(targetSize.height / 2)
+                        }
+                        .onGloballyPositioned {
+                            targetSize = it.size
+                        }
+                ) {
+                    state.draggableComposable?.invoke()
+                }
+            }
+        }
+    }
 }
 
 internal class DragTargetInfo {
@@ -68,5 +133,5 @@ internal class DragTargetInfo {
     var dragPosition by mutableStateOf(Offset.Zero)
     var dragOffset by mutableStateOf(Offset.Zero)
     var draggableComposable by mutableStateOf<(@Composable () -> Unit)?>(null)
-    var dayaToDrop by mutableStateOf<Any?>(null)
+    var dataToDrop by mutableStateOf<Any?>(null)
 }
---
+++ app/src/main/java/com/example/draganddrop/MainViewModel.kt
@@ -0,0 +1,40 @@
+package com.example.draganddrop
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+    var isCurrentlyDragging by mutableStateOf(false)
+        private set
+
+    var items by mutableStateOf(emptyList<PersonUiItem>())
+        private set
+
+    var addedPersons = mutableStateListOf<PersonUiItem>()
+        private set
+
+    init {
+        items = listOf(
+            PersonUiItem("Tamerlan", "1", Color.Gray),
+            PersonUiItem("Ariz", "2", Color.Blue),
+            PersonUiItem("Elcin", "3", Color.Green)
+        )
+    }
+
+    fun startDragging() {
+        isCurrentlyDragging = true
+    }
+
+    fun stopDragging() {
+        isCurrentlyDragging = false
+    }
+
+    fun addPerson(personUiItem: PersonUiItem) {
+        addedPersons.add(personUiItem)
+    }
+}
